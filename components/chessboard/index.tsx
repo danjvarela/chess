de91d2799bb56b72c366fa963f0ed7ebeb268828
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-10-05 23:06:24 +0800

    add engine-game-settings hooks

--- components/chessboard/index.tsx
+++ components/chessboard/index.tsx
@@ -1,13 +1,10 @@
-import { headers } from "next/headers"
-import { getSelectorsByUserAgent } from "react-device-detect"
+"use client"
+
+import { isMobile } from "react-device-detect"
 import MobileChessboard from "./chessboard.mobile"
 import BrowserChessboard from "./chessboard.browser"
 
 export default function Chessboard({ unplayable }: { unplayable?: boolean }) {
-  const { isMobile } = getSelectorsByUserAgent(
-    headers().get("user-agent") ?? ""
-  )
-
   if (unplayable) {
     return (
       <div className="h-full w-full relative">
---
+++ hooks/engine-game-settings.ts
@@ -0,0 +1,62 @@
+import { create } from "zustand"
+import { persist, subscribeWithSelector } from "zustand/middleware"
+import omit from "lodash/omit"
+import { Color } from "chess.js"
+import usePersistStore from "./persist-store"
+
+const DEFAULT_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+const DEFAULT_ENGINE_DIFFICULTY = 2
+
+export type EngineGameSettings = {
+  fen: string
+  gameStarted: boolean
+  playerColor: Color
+  gameOver: boolean
+  difficulty: number
+
+  actions: {
+    setFen: (fen: string) => void
+    setGameStarted: (gameStarted: boolean) => void
+    setPlayerColor: (playerColor: Color) => void
+    setGameOver: (gameOver: boolean) => void
+    setDifficulty: (difficulty: number) => void
+  }
+}
+
+const useEngineGameStore = create<EngineGameSettings>()(
+  subscribeWithSelector(
+    persist(
+      (set) => ({
+        fen: DEFAULT_FEN,
+        gameStarted: false,
+        difficulty: DEFAULT_ENGINE_DIFFICULTY,
+        playerColor: "w",
+        gameOver: false,
+        actions: {
+          setFen: (fen: string) => set({ fen }),
+          setGameStarted: (gameStarted: boolean) => set({ gameStarted }),
+          setPlayerColor: (playerColor: Color) => set({ playerColor }),
+          setGameOver: (gameOver: boolean) => set({ gameOver }),
+          setDifficulty: (difficulty: number) => set({ difficulty }),
+        },
+      }),
+      {
+        name: "chess_engine_game_settings",
+        partialize: (state) => omit(state, ["actions"]),
+      }
+    )
+  )
+)
+
+export const useEngineFen = () => {
+  const data = usePersistStore(useEngineGameStore, (state) => ({
+    fen: state.fen,
+    setFen: state.actions.setFen,
+  }))
+
+  if (!data) {
+    return { fen: undefined, setFen: () => {} }
+  }
+
+  return data
+}
