301919cd962bdeac86186e3bc950ce79343add41
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-10-06 00:53:42 +0800

    show possible moves on the browser chessboard

--- components/chessboard/chessboard.browser.tsx
+++ components/chessboard/chessboard.browser.tsx
@@ -9,7 +9,7 @@ import {
   Square,
 } from "react-chessboard/dist/chessboard/types"
 import { useGame } from "@/hooks/game"
-import { kingCheckedStyle, sharedProps } from "./sharedProps"
+import { kingCheckedStyle, possibleMoveStyle, sharedProps } from "./sharedProps"
 import { useIsClient } from "usehooks-ts"
 import Loading from "@/app/loading"
 
@@ -18,6 +18,38 @@ export default function BrowserChessboard() {
   const [customSquareStyles, setCustomSquareStyles] =
     useState<CustomSquareStyles>({})
   const isClient = useIsClient()
+
+  const handleOnSquareClick = useCallback(
+    (square: Square) => {
+      const pieceOnClickedSquare = game.get(square)
+
+      // set moveFrom
+      // highlight the clicked square
+      // highlight possible squares
+      if (pieceOnClickedSquare.color === game.turn()) {
+        const possibleSquaresStyle = game
+          .moves({ square, verbose: true })
+          .map((move) => move.to)
+          .reduce(
+            (acc, square) => ({
+              ...acc,
+              [square]: possibleMoveStyle,
+            }),
+            {}
+          )
+        setCustomSquareStyles(possibleSquaresStyle)
+      }
+    },
+    [game]
+  )
+
+  const handleOnPieceDragBegin = (_: Piece, square: Square) => {
+    handleOnSquareClick(square)
+  }
+
+  const handleOnPieceDragEnd = () => {
+    setCustomSquareStyles({})
+  }
 
   const handlePieceDrop = useCallback(
     (sourceSquare: Square, targetSquare: Square, piece: Piece) => {
@@ -57,6 +89,9 @@ export default function BrowserChessboard() {
     <ReactChessboard
       position={game.fen()}
       onPieceDrop={handlePieceDrop}
+      onPieceDragEnd={handleOnPieceDragEnd}
+      onPieceDragBegin={handleOnPieceDragBegin}
+      onSquareClick={handleOnSquareClick}
       customSquareStyles={customSquareStyles}
       {...sharedProps}
     />
