1418904aa2d5d46f2a20ff2a822ce208365499c7
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-10-07 00:54:33 +0800

    use react-chessboard props for unplayable chessboards

--- components/chessboard/index.tsx
+++ components/chessboard/index.tsx
@@ -3,14 +3,19 @@
 import { isMobile } from "react-device-detect"
 import MobileChessboard from "./chessboard.mobile"
 import BrowserChessboard from "./chessboard.browser"
+import { Chessboard as ReactChessboard } from "react-chessboard"
+import { sharedProps } from "./sharedProps"
+import { useIsClient } from "usehooks-ts"
+import Loading from "@/app/loading"
 
 export default function Chessboard({ unplayable }: { unplayable?: boolean }) {
+  const isClient = useIsClient()
+
   if (unplayable) {
-    return (
-      <div className="h-full w-full relative">
-        <BrowserChessboard />
-        <div className="absolute top-0 w-full h-full" />
-      </div>
+    return isClient ? (
+      <ReactChessboard arePiecesDraggable={false} areArrowsAllowed={false} {...sharedProps} />
+    ) : (
+      <Loading />
     )
   } else {
     return isMobile ? <MobileChessboard /> : <BrowserChessboard />
