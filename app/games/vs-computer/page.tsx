d9c8e5b0a50a560fb0649bbcfa3b48e10eee8af6
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-10-12 01:32:03 +0800

    put game settings in a dialog

--- app/games/vs-computer/page.tsx
+++ app/games/vs-computer/page.tsx
@@ -2,30 +2,32 @@
 
 import Chessboard from "@/components/chessboard"
 import GameSettings from "@/components/game-settings"
+import cn from "@/utils/cn"
 import { Card } from "@radix-ui/themes"
 
 export default function VsComputer() {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center py-8 px-2 gap-4">
-      <div className="flex w-full justify-between max-w-[730px]">
-        <Card size="1" className="w-full flex justify-center">
-          <span className="font-bold text-xs md:text-base">Computer</span>
-        </Card>
+    <div
+      className={cn(
+        "w-full h-screen flex flex-col justify-center items-center py-8 px-2 gap-4",
+        "md:max-w-2xl mx-auto"
+      )}
+    >
+      <div className="flex justify-end w-full">
+        <GameSettings />
       </div>
 
-      <div className="relative w-full max-w-[730px] min-h-[730px] bg-sage-50">
+      <Card size="1" className="w-full flex justify-center">
+        <span className="font-bold text-xs md:text-base">Computer</span>
+      </Card>
+
+      <div className="w-full aspect-square bg-sage-50">
         <Chessboard mode="vsEngine" />
-
-        <div className="absolute w-[250px] left-full inset-0 ml-8 flex flex-col gap-8">
-          <GameSettings />
-        </div>
       </div>
 
-      <div className="flex w-full justify-between max-w-[730px]">
-        <Card size="1" className="w-full flex justify-center">
-          <span className="font-bold text-xs md:text-base">You</span>
-        </Card>
-      </div>
+      <Card size="1" className="w-full flex justify-center">
+        <span className="font-bold text-xs md:text-base">You</span>
+      </Card>
     </div>
   )
 }
--- components/game-settings.tsx
+++ components/game-settings.tsx
@@ -2,6 +2,8 @@ import { Button, Slider, TextField } from "@radix-ui/themes"
 import { Color } from "chess.js"
 import clamp from "lodash/clamp"
 import { useState } from "react"
+import { Dialog } from "@radix-ui/themes"
+import { AiOutlineSetting } from "react-icons/ai"
 
 const MIN_ENGINE_DIFFICULTY = 2
 const MAX_ENGINE_DIFFICULTY = 24
@@ -11,63 +13,73 @@ export default function GameSettings() {
   const [playerColor, setPlayerColor] = useState<Color>("w")
 
   return (
-    <>
-      <div className="flex flex-col gap-4 w-full">
-        <span>Difficulty</span>
-        <div className="flex gap-4 items-center">
-          <Slider
-            value={[difficulty]}
-            min={MIN_ENGINE_DIFFICULTY}
-            max={MAX_ENGINE_DIFFICULTY}
-            className="flex-1"
-            onValueChange={(value) => {
-              setDifficulty(value[0])
-            }}
-          />
-          <TextField.Root className="w-16">
-            <TextField.Input
-              type="number"
-              value={difficulty}
-              onChange={(e) => {
-                const num = parseInt(e.target.value)
-                setDifficulty(
-                  clamp(num, MIN_ENGINE_DIFFICULTY, MAX_ENGINE_DIFFICULTY)
-                )
-              }}
-              max={MAX_ENGINE_DIFFICULTY}
-              min={MIN_ENGINE_DIFFICULTY}
-            />
-          </TextField.Root>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-4 w-full">
-        <span>Play as</span>
-        <div className="flex w-full gap-4">
-          <Button
-            className="flex-1"
-            variant={playerColor === "w" ? "solid" : "outline"}
-            onClick={() => setPlayerColor("w")}
-          >
-            White
-          </Button>
-          <Button
-            className="flex-1"
-            variant={playerColor === "b" ? "solid" : "outline"}
-            onClick={() => setPlayerColor("b")}
-          >
-            Black
-          </Button>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-4 w-full mt-4">
-        <Button size="3">Start Game</Button>
-        <Button size="3">New Game vs a Friend</Button>
-        <Button size="3" color="gray" variant="soft">
-          Abort Game
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button>
+          <AiOutlineSetting />
+          Game Settings
         </Button>
-      </div>
-    </>
+      </Dialog.Trigger>
+      <Dialog.Content className="!max-w-xs">
+        <div className="flex flex-col gap-4 w-full items-center">
+          <div className="flex flex-col gap-4 w-full">
+            <span>Difficulty</span>
+            <div className="flex gap-4 items-center">
+              <Slider
+                value={[difficulty]}
+                min={MIN_ENGINE_DIFFICULTY}
+                max={MAX_ENGINE_DIFFICULTY}
+                className="flex-1"
+                onValueChange={(value) => {
+                  setDifficulty(value[0])
+                }}
+              />
+              <TextField.Root className="w-16">
+                <TextField.Input
+                  type="number"
+                  value={difficulty}
+                  onChange={(e) => {
+                    const num = parseInt(e.target.value)
+                    setDifficulty(
+                      clamp(num, MIN_ENGINE_DIFFICULTY, MAX_ENGINE_DIFFICULTY)
+                    )
+                  }}
+                  max={MAX_ENGINE_DIFFICULTY}
+                  min={MIN_ENGINE_DIFFICULTY}
+                />
+              </TextField.Root>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 w-full">
+            <span>Play as</span>
+            <div className="flex w-full gap-4">
+              <Button
+                className="flex-1"
+                variant={playerColor === "w" ? "solid" : "outline"}
+                onClick={() => setPlayerColor("w")}
+              >
+                White
+              </Button>
+              <Button
+                className="flex-1"
+                variant={playerColor === "b" ? "solid" : "outline"}
+                onClick={() => setPlayerColor("b")}
+              >
+                Black
+              </Button>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 w-full mt-4">
+            <Button size="3">Start Game</Button>
+            <Button size="3">New Game vs a Friend</Button>
+            <Button size="3" color="gray" variant="soft">
+              Abort Game
+            </Button>
+          </div>
+        </div>
+      </Dialog.Content>
+    </Dialog.Root>
   )
 }
