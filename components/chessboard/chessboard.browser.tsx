2c56a43d3ad75f119741f6c841a70ffeed143c8a
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-10-19 01:28:35 +0800

    enable vs-engine gameplay

--- components/chessboard/chessboard.browser.tsx
+++ components/chessboard/chessboard.browser.tsx
@@ -1,5 +1,5 @@
 import { Chessboard as ReactChessboard } from "react-chessboard"
-import { useCallback, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import {
   CustomSquareStyles,
   Piece,
@@ -8,93 +8,112 @@ import {
 import { kingCheckedStyle, possibleMoveStyle, sharedProps } from "./sharedProps"
 import { ChessboardProps } from "./types"
 import { Chess } from "chess.js"
-import { useFen } from "@/hooks/games"
+import { useEngineColor, useEngineDifficulty, useFen } from "@/hooks/games"
 import Spinner from "../ui/spinner"
+import { useEngine } from "@/hooks/engine"
+import { safeMove } from "@/utils/games"
 
 export default function BrowserChessboard({
   mode,
   id,
   ...props
 }: ChessboardProps) {
+  const engineColor = useEngineColor(id)
   const { fen, setFen } = useFen(id)
   const [game, setGame] = useState<Chess>()
+  const { engine, executeEngineMove } = useEngine()
+  const engineDifficulty = useEngineDifficulty(id)
   const [customSquareStyles, setCustomSquareStyles] =
     useState<CustomSquareStyles>({})
 
-  const handleOnSquareClick = useCallback(
-    (square: Square) => {
-      const pieceOnClickedSquare = game?.get(square)
+  const handleOnSquareClick = (square: Square) => {
+    if (!game) return
+    const pieceOnClickedSquare = game.get(square)
 
-      // set moveFrom
-      // highlight the clicked square
-      // highlight possible squares
-      if (pieceOnClickedSquare?.color === game?.turn()) {
-        const possibleSquaresStyle = game
-          ?.moves({ square, verbose: true })
-          .map((move) => move.to)
-          .reduce(
-            (acc, square) => ({
-              ...acc,
-              [square]: possibleMoveStyle,
-            }),
-            {}
-          )
-        setCustomSquareStyles(possibleSquaresStyle ?? {})
-      }
-    },
-    [game]
-  )
+    // set moveFrom
+    // highlight the clicked square
+    // highlight possible squares
+    if (pieceOnClickedSquare.color === game.turn()) {
+      const possibleSquaresStyle = game
+        .moves({ square, verbose: true })
+        .map((move) => move.to)
+        .reduce(
+          (acc, square) => ({
+            ...acc,
+            [square]: possibleMoveStyle,
+          }),
+          {}
+        )
+      setCustomSquareStyles(possibleSquaresStyle ?? {})
+    }
+  }
 
   const handleOnPieceDragBegin = (_: Piece, square: Square) => {
     handleOnSquareClick(square)
   }
 
-  const handlePieceDrop = useCallback(
-    (sourceSquare: Square, targetSquare: Square, piece: Piece) => {
-      if (!game) return false
-      try {
-        game?.move({
-          from: sourceSquare,
-          to: targetSquare,
-          promotion: piece[1].toLowerCase() ?? "q",
-        })
+  const handlePieceDrop = (
+    sourceSquare: Square,
+    targetSquare: Square,
+    piece: Piece
+  ) => {
+    if (!game) return false
 
-        setGame(new Chess(game?.fen()))
-        setCustomSquareStyles({})
-        return true
-      } catch (err) {
-        if (game?.inCheck()) {
-          const kingSquare = game
-            .board()
-            .flat()
-            .find(
-              (position) =>
-                position?.color === game.turn() && position.type === "k"
-            )?.square
-          if (!kingSquare) return false
+    const move = safeMove(game, {
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: piece[1].toLowerCase() ?? "q",
+    })
 
-          setCustomSquareStyles({ [kingSquare]: kingCheckedStyle })
-        }
-        return false
+    // if the move was illegal, we check if the king is checked
+    if (move === null) {
+      if (game.inCheck()) {
+        const kingSquare = game
+          .board()
+          .flat()
+          .find(
+            (position) =>
+              position?.color === game.turn() && position.type === "k"
+          )?.square
+        if (!kingSquare) return false
+
+        setCustomSquareStyles({ [kingSquare]: kingCheckedStyle })
       }
-    },
-    [game]
-  )
+      return false
+    }
+
+    setGame(new Chess(game.fen()))
+    setCustomSquareStyles({})
+
+    // if mode is vsEngine, execute engine's move
+    if (mode === "vsEngine" && engineColor) {
+      setTimeout(
+        () =>
+          executeEngineMove({
+            gameState: [game, setGame],
+            engine,
+            color: engineColor,
+            difficulty: engineDifficulty,
+          }),
+        300
+      )
+    }
+
+    return true
+  }
 
   useEffect(() => {
     // use the databse to initialize game
-    if (fen && !game) {
-      setGame(new Chess(fen))
-    }
-  }, [fen])
+    if (!fen || game) return
+    setGame(new Chess(fen))
+  }, [fen, game])
 
   useEffect(() => {
     // when the game is initialized,
     // update the fen in the database every move
-    if (game) {
-      setFen(game.fen())
-    }
-  }, [game])
+    if (!game) return
+    setFen(game.fen())
+  }, [game, setFen])
 
   if (!game)
     return (
--- components/chessboard/index.tsx
+++ components/chessboard/index.tsx
@@ -4,38 +4,11 @@ import { isMobile } from "react-device-detect"
 import MobileChessboard from "./chessboard.mobile"
 import BrowserChessboard from "./chessboard.browser"
 import { ChessboardProps } from "./types"
-import { useUpdateGame } from "@/hooks/games"
-import { PENDING_PLAYER } from "@/utils/constants"
-import { Color } from "chess.js"
-import { useSession } from "next-auth/react"
-import { useMemo } from "react"
 
 export default function Chessboard({
   mode = "vsFriend",
   id,
 }: Pick<ChessboardProps, "mode" | "id">) {
-
-  // const playerColor: Color = useMemo(() => {
-  //   const playerId = session?.data?.user?.id
-  //
-  //   if (gameData?.mode === "vsEngine") {
-  //     return gameData?.whitePlayer === playerId ? "w" : "b"
-  //   } else {
-  //     if (gameData?.whitePlayer === playerId) return "w"
-  //     if (gameData?.blackPlayer === playerId) return "b"
-  //
-  //     // logged in player is neither black nor white so we populate the pending player
-  //     // with the logged in player's id
-  //     if (gameData?.whitePlayer === PENDING_PLAYER) {
-  //       updateGame({ whitePlayer: playerId })
-  //       return "w"
-  //     } else {
-  //       updateGame({ blackPlayer: playerId })
-  //       return "b"
-  //     }
-  //   }
-  // }, [session, gameData])
-
   const chessboardProps: ChessboardProps = {
     id,
     mode,
