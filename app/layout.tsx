988a0fbf6ed96a53ccf7e46e7a96338db82e517a
Dan Varela <dan@danvarela.com>
2023-11-04 05:00:28 +0800

    replace next-ui with shadcn-ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import Providers from "./providers"
-import { cn } from "@/utils/cn"
+import { cn } from "@/lib/utils"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,15 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          inter.className,
-          "dark text-foreground bg-background min-h-screen"
-        )}
-      >
-        <Providers>{children}</Providers>
-      </body>
+    <html lang="en" className="dark">
+      <body className={cn(inter.className, "min-h-screen")}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <div className="container mx-auto border h-full">
-     hello 
+      <Button>hello</Button>
     </div>
   )
 }
